e5744b6202b00e880a98c55bf16388a43985b93b
Michelle Ross <miross629@gmail.com>
2023-01-10 15:23:28 -0800

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header.js';
 import Main from './components/Main/Main.js';
 
 function App() {
-  return <Main />;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
