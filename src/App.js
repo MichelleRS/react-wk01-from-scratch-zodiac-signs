48ede10dce46dc3010fa928779a74b73f10f7fab
Michelle Ross <miross629@gmail.com>
2023-01-10 15:29:59 -0800

    Add footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header.js';
 import Main from './components/Main/Main.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>Copyright Michelle 2023</p>
+    </footer>
+  );
+}
